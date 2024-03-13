03ba7386d157e6ae265dcd4027db1f582c04fe9d
Tatsuya Nibuno <nibu10.majik@gmail.com>
2024-03-13 20:35:06 +0900

    テキストを受け取れるようにした

--- emoemo/views.py
+++ emoemo/views.py
@@ -1,4 +1,7 @@
 from django.shortcuts import render
 
+
 def index(request):
-    return render(request, "base.html")
+    font_text = request.GET.get("font-text")
+
+    return render(request, "base.html", {"font_text": font_text})
