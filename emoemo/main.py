69510e190a1e423bdbafda0e4e526f6fda3f9202
Tatsuya Nibuno <nibu10.majik@gmail.com>
2024-03-20 21:24:03 +0900

    format実行

--- emoemo/main.py
+++ emoemo/main.py
@@ -7,7 +7,12 @@ from emoemo.infrastructure.generator import (
 from emoemo.use_case.emoji_use_case import EmojiUseCase
 
 
-def main(input_text: str, auto_font_size: bool, font_color: str = "#000000", font_name: str = "rounded-mplus-1c-medium"):
+def main(
+    input_text: str,
+    auto_font_size: bool,
+    font_color: str = "#000000",
+    font_name: str = "rounded-mplus-1c-medium",
+):
     # FIXME: 背景色、フォントの色を変更できるようにする
     emoji = Emoji(text=input_text, font_color=font_color, font_name=font_name)
     emoji_use_case = EmojiUseCase(emoji)
