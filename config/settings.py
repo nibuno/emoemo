bcc4b3d9e0b51d65472752db93423752d128fa3d
Tatsuya Nibuno <nibu10.majik@gmail.com>
2024-04-06 14:42:07 +0900

    コンテナ上で動かせるようにアドレス追加

--- config/settings.py
+++ config/settings.py
@@ -29,7 +29,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = str(os.getenv("SECRET_KEY"))
 DEBUG = True
 
-ALLOWED_HOSTS = ["0.0.0.0"]
+ALLOWED_HOSTS = ["0.0.0.0" , "127.0.0.1"]
 
 
 # Application definition
