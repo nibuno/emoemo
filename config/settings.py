a3956ecd6fe14916d40296694c104b4ee8a288cd
Tatsuya Nibuno <nibu10.majik@gmail.com>
2024-06-01 17:21:21 +0900

    元々の定数値と同じように修正

--- config/settings.py
+++ config/settings.py
@@ -134,7 +134,7 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
-STATIC_URL = '/static/'
+STATIC_URL = 'static/'
 STATIC_ROOT = '/emoemo/static/'
 
 CSRF_TRUSTED_ORIGINS = ["http://localhost:1337"]
